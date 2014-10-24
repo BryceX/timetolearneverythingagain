3994eb7a47d029cd3d06e8de998dff21e07b5ce1
BryceX <btau20102@yahoo.com>
2014-10-24 12:33:26 -0700

    bleh
    asdf

--- AIE Year1 Framework VS2013/source/main.cpp
+++ AIE Year1 Framework VS2013/source/main.cpp
@@ -1,32 +1,177 @@
 ﻿#include "AIE.h"
 #include <iostream>
-
-const int screenWidth = 672;
-const int screenHeight = 780;
+#include <cstdlib> 
+const int iScreenWidth = 672;
+const int iScreenHeight = 780;
 const char* pInvadersFont = "./fonts/invaders.fnt";
 char score1[10] = "00000";
 char highScore[10] = "00000";
 char score2[10] = "00000";
 char credit[10] = "99";
 char insertCoins[20] = "Insert Coins";
-int playerWidth = 64;
-int playerHeight = 32;
+
+unsigned int iArcadeMarquee;
+
+
+unsigned int alienShips[18];
+
+struct PlayerCannon
+{
+	unsigned int iSpriteID;
+	float fWidth;
+	float fHeight;
+	void SetSize(float a_fWidth, float a_fHeight)
+	{
+		fWidth = a_fWidth;
+		fHeight = a_fHeight;
+	}
+
+	float x;
+	float y;
+	void SetPosition(float a_x, float a_y)
+	{
+		x = a_x;
+		y = a_y;
+	}
+
+	unsigned int iMoveLeftKey;
+	unsigned int iMoveRightKey;
+	void SetMovementKeys(unsigned int a_moveLeft, unsigned int a_moveRight)
+	{
+		iMoveLeftKey = a_moveLeft;
+		iMoveRightKey = a_moveRight;
+	}
+	unsigned int iLeftMovementExtreme;
+	unsigned int iRightMovementExtreme;
+	void SetMovementExtremes(unsigned int a_leftExtreme, unsigned int a_RightExtreme)
+	{
+		iLeftMovementExtreme = a_leftExtreme;
+		iRightMovementExtreme = a_RightExtreme;
+	}
+
+	void Move(float a_fTimeStep, float a_fSpeed)
+	{
+		if (IsKeyDown(iMoveLeftKey))
+		{
+			x -= a_fTimeStep * a_fSpeed;
+			if (x < (iLeftMovementExtreme + fWidth*.5f))
+			{
+				x = (iLeftMovementExtreme + fWidth*.5f);
+			}
+		}
+		if (IsKeyDown(iMoveRightKey))
+		{
+			x += a_fTimeStep * a_fSpeed;
+			if (x >(iRightMovementExtreme - fWidth*.5f))
+			{
+				x = (iRightMovementExtreme - fWidth*.5f);
+			}
+		}
+		MoveSprite(iSpriteID, x, y);
+	}
+};
+struct Enemy
+{
+	int eLeft;
+	int eRight;
+	int eDown;
+	int fEnemyX = iScreenWidth * 0.2;
+	int fEnemyY = iScreenHeight * 0.7;
+	int enemyWidth = 64;
+	int enemyHeight = 32;
+	void CreateEnemies()
+	{
+		
+		for (int i = 0; i < 18; ++i)
+		{
+			alienShips[i] = CreateSprite("./images/invaders/invaders_1_00.png", enemyWidth, enemyHeight, true);
+			MoveSprite(alienShips[i], fEnemyX, fEnemyY);
+			fEnemyX += 0.12 * iScreenWidth;
+			if (fEnemyX > iScreenWidth * 0.8f)
+			{
+				fEnemyX = iScreenWidth * 0.2f;
+				fEnemyY -= 0.08 * iScreenHeight;
+			}
+		}
+
+	}
+		bool Move(float a_fDeltaTime, int a_Direction)
+		{
+			if (a_Direction == eLeft)
+			{
+				fEnemyX -= GetDeltaTime()*.1;
+				if ( fEnemyX < enemyWidth*.5)
+				{
+					fEnemyX = enemyWidth*.5;
+					fEnemyY -= iScreenHeight*.05;
+					a_Direction = eRight;
+					return true;
+				}
+			}
+			if (a_Direction == eRight)
+			{
+				fEnemyX += GetDeltaTime()*.1;
+				if ( fEnemyX >iScreenWidth - enemyWidth)
+				{
+					fEnemyX = iScreenWidth - enemyWidth;
+					fEnemyY -= iScreenHeight*.05;
+					return true;
+				}
+			}
+			
+	}
+};
+
 
 enum GAMESTATES{
 	MAINMENU,
 	GAMEPLAY,
 	GAMEOVER
 };
+void UpdateMainMenu()
+{
+	MoveSprite(iArcadeMarquee, iScreenWidth*.5, iScreenHeight*.5);
+	DrawSprite(iArcadeMarquee);
+	DrawString(insertCoins, iScreenWidth * 0.37, iScreenHeight * 0.5);
+	DrawString(credit, iScreenWidth * 0.25, iScreenHeight * 0.4);
+}
+void UpdateGameState()
+{
+	
+	
+	for (int i = 0; i < 18; ++i)
+	{
+		DrawSprite(alienShips[i]);
+	}
+	DrawLine(0, 40, iScreenWidth, 40, SColour(0x00, 0xFC, 0x00, 0xFF));
+	SetFont(pInvadersFont);
+	DrawString(score1, iScreenWidth * 0.025f, iScreenHeight - 2);
+	DrawString(highScore, iScreenWidth * 0.425f, iScreenHeight - 2);
+	DrawString(score2, iScreenWidth * 0.8f, iScreenHeight - 2);
+}
+
 int main( int argc, char* argv[] )
 {	
     Initialise(672, 780, false, "My Awesome Game");
+	AddFont(pInvadersFont);
+	SetFont(pInvadersFont);
 	
-	unsigned int iPlayerCannon = CreateSprite("./images/cannon.png", 64, 32, true);
-	unsigned int iArcadeMarquee = CreateSprite("./images/Space-Invaders-Marquee.png", screenWidth, screenHeight, true);
+	iArcadeMarquee = CreateSprite("./images/Space-Invaders-Marquee.png", iScreenWidth, iScreenHeight, true);
 	
-	float fPlayerX = screenWidth * 0.5f;
-	float fPlayerY = 80.f;
-	MoveSprite(iPlayerCannon, fPlayerX, fPlayerY);
+
+	PlayerCannon player;
+	player.fHeight = 32.f;
+	player.fWidth = 64.f;
+	player.iSpriteID = CreateSprite("./images/cannon.png", player.fWidth, player.fHeight, true);
+	player.x = iScreenWidth * 0.5;
+	player.y = 80;
+	player.SetSize(64, 32);
+	player.SetPosition(iScreenWidth*.5, 80);
+	player.SetMovementExtremes(0, 672);
+	player.SetMovementKeys('A', 'D');
+	
+	
+	
 	SetBackgroundColour(SColour(0x00, 0x00, 0x00, 0xFF));
 
     
@@ -38,50 +183,51 @@ int main( int argc, char* argv[] )
 		switch (currentState)
 		{
 		case MAINMENU:
-			MoveSprite(iArcadeMarquee, screenWidth*.5, screenHeight*.5);
-			DrawSprite(iArcadeMarquee);
-			DrawString(insertCoins, screenWidth * 0.37, screenHeight * 0.5);
-			DrawString(credit, screenWidth * 0.25, screenHeight * 0.4);
-			ClearScreen();
-			if (IsKeyDown(KEY_ENTER))
+			system("CLS");
+			AddFont(pInvadersFont);
+			SetFont(pInvadersFont);
+			UpdateMainMenu();
+			
+			
+			if (IsKeyDown(GLFW_KEY_ENTER))
 			{
 				currentState = GAMEPLAY;
 			}
+			system("CLS");
+			
+			
 			break;
 
 
 
 		case GAMEPLAY:
-			if (IsKeyDown('A'))
+			system("CLS");
+			UpdateGameState();
+			player.Move(GetDeltaTime(), iScreenWidth*.2);
+			MoveSprite(player.iSpriteID, player.x, player.y);
+			DrawSprite(player.iSpriteID);
+			if (IsKeyDown(GLFW_KEY_RIGHT_SHIFT))
 			{
-				fPlayerX -= 2.f;
-				if (fPlayerX < (playerWidth*.5f))
-				{
-					fPlayerX = (playerWidth*.5f);
-				}
+				currentState = GAMEOVER;
 			}
-			MoveSprite(iPlayerCannon, fPlayerX, fPlayerY);
-			DrawSprite(iPlayerCannon);
-
-			DrawLine(0, 40, screenWidth, 40, SColour(0x00, 0xFC, 0x00, 0xFF));
-
-			SetFont(pInvadersFont);
-			DrawString(score1, screenWidth * 0.025f, screenHeight - 2);
-			DrawString(highScore, screenWidth * 0.425f, screenHeight - 2);
-			DrawString(score2, screenWidth * 0.8f, screenHeight - 2);
-			DrawString(credit, screenWidth * 0.7f, 38);
+			ClearScreen();
 			break;
 
 		case GAMEOVER:
+			ClearScreen();
+			if (IsKeyDown(GLFW_KEY_ENTER))
+			{
+				currentState = MAINMENU;
+			}
+			ClearScreen();
 			break;
-
-
 		}
 		
        
 
-    } while(!FrameworkUpdate());
-	DestroySprite(iPlayerCannon);
+	} while (FrameworkUpdate() == false);
+	DestroySprite(player.iSpriteID);
+	
     Shutdown();
 
     return 0;
